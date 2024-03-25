60b9c14d742169d7892518ecaa86e60dc674e47c
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-03-26 07:50:31 +0800

    change active link color, remove 2 products and change a text spelling in product page

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -1,5 +1,4 @@
 import Directory from "../assets/images/directory.png";
-import Imbakan from "../assets/images/imbakan.png";
 import Matuto from "../assets/images/matuto.png";
 
 const productsData = [
@@ -12,26 +11,11 @@ const productsData = [
     buttonValue: "Submit Project",
   },
   {
-    title: "Imbakan",
-    briefDescription:
-      "Imbakan is your digital storage ally, offering a seamless Edge Gateway experience to securely store, manage, and access your files with a simple CID input.",
-    url: "#",
-    image: Imbakan,
-    buttonValue: "Explore Gateway",
-  },
-  {
     title: "Matuto",
     briefDescription:
       "Transform every new tab into a learning adventure with Matuto, the simple web browser extension that brings Web3 education to your fingertips.",
     url: "https://github.com/Web3Philippines/matuto",
     image: Matuto,
-    buttonValue: "Install Extension",
-  },
-  {
-    title: "Bantay",
-    briefDescription: `A simple browser extension for learning web3 in every new tab. It's a Filipino word which means "to learn". Which is the goal of this project, to help people learn more about web3, blockchain and cryptocurrencies.`,
-    url: "#",
-    image: Directory,
     buttonValue: "Install Extension",
   },
 ];
