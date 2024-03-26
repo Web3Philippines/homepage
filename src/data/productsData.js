387eb5a307731321956046cb57c897fffaf7f561
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-03-26 11:10:20 +0800

    tweak: fix layout of products on homepage

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -3,20 +3,20 @@ import Matuto from "../assets/images/matuto.png";
 
 const productsData = [
   {
-    title: "Directory",
-    briefDescription:
-      "Discover the Web3 Philippines Directory, the community-curated digital gateway to the Philippines' Web3 resources and projects.",
-    url: "https://github.com/Web3Philippines/directory",
-    image: Directory,
-    buttonValue: "Submit Project",
-  },
-  {
     title: "Matuto",
     briefDescription:
       "Transform every new tab into a learning adventure with Matuto, the simple web browser extension that brings Web3 education to your fingertips.",
     url: "https://github.com/Web3Philippines/matuto",
     image: Matuto,
     buttonValue: "Install Extension",
+  },
+  {
+    title: "Directory",
+    briefDescription:
+      "Discover the Web3 Philippines Directory, the community-curated digital gateway to the Philippines' Web3 resources and projects.",
+    url: "https://github.com/Web3Philippines/directory",
+    image: Directory,
+    buttonValue: "Submit Project",
   },
 ];
 
