da78f0347b6c8f219091b325590d24d2028e13f5
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-04-01 15:15:49 +0800

    add hotjar using script in index.html instead

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import Hotjar from "@hotjar/browser";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-const siteId = 3926974;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
