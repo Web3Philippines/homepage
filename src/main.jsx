69f1c041d6ab195b27b3e69dc709060f90002100
JB Benedicto <jbenedicto20.jbb@gmail.com>
2024-04-01 15:11:42 +0800

    add hotjar initializer

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Hotjar from "@hotjar/browser";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const siteId = 3926974;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
